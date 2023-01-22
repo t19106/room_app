47b6912fa1293da641a215945d5c074a2cc9e829
t19106 <g037sk@gmail.com>
2023-01-22 20:19:21 +0900

    fix search controller

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,13 @@
 class SearchesController < ApplicationController
+  before_action :check_login
+
   def search
     @records = Room.search_by_area(params[:area]) if params[:genre] == "area"
     @records = Room.search_by_content(params[:content]) if params[:genre] == "content"
+  end
+
+  private
+  def check_login
+    redirect_to login_path unless current_user.login?
   end
 end
