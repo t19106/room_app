63a85ddbe51eeb73107248fb51dc39fee56bf796
t19106 <g037sk@gmail.com>
2023-01-22 10:46:02 +0900

    create controller users new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get  '/signup',  to: 'users#new'
+  resources :users, only: :create
 end
